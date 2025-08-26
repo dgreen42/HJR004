a7dadc445781f5a1feeac424b912463affee81a3
David <lazgreen33@gmail.com>
2025-08-26 16:18:25 -0400

    no work

--- work/65/0e2a696b71505843c337213412d914/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-minimap2 --version | sed 's/^/minimap2,/' >> tool_versions.txt
-samtools --version | head -n1 >> tool_versions.txt
-R --version | head -n1 >> tool_versions.txt
--- work/72/72457f8875c24e6628657db812304c/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-minimap2 -t 4 -d "index.mmi" MtrunA17r5.0-ANR-EGN-r1.9.gtf
